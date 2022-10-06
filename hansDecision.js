e0f872dd653e5b8faef73c5753f6bdb50db939f8
Hans-tech-coder <obenahans@gmail.com>
2022-10-06 21:15:34 +0800

    if and else function

---
+++ hansDecision.js
@@ -0,0 +1,10 @@
+var count = 4;
+    if(count == 4){
+        console.log('count is equal to 4');
+    }else if(count > 4) {
+        console.log('count is greaterthan 4');
+    }else if(count < 4) {
+        console.log('count is lessthan 4');
+    }else{
+        console.log('count is not equal to 4');
+    }
