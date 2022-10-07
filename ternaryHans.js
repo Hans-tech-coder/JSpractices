43cfa5b94b031c9d8a1fc834cd5f9e0790222b09
Hans-tech-coder <obenahans@gmail.com>
2022-10-07 21:04:22 +0800

    ternary add ternaHAns.js

---
+++ ternaryHans.js
@@ -0,0 +1,10 @@
+let a = 1, b = "1";
+let c = a + b;
+let result = (a === b) ? "true" : "false";
+
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof result);
+console.log(result);
