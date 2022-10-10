fca7ba921e68a9f6f6ec6dcc2da9d25b9181ffb8
Hans-tech-coder <obenahans@gmail.com>
2022-10-10 20:43:28 +0800

    add array eleemnts in the car elements

--- object-literal.js
+++ object-literal.js
@@ -3,14 +3,15 @@
   objects will be one data that will attribute  
 */
 
-// let car = ['year' , 'make' , 'model']
+let carOne = ['year' , 'make' , 'model']
 
 let car = {
   make: 'nissan',
-  model: 'juke',
+  // model: 'juke'; - this a first example for the model of the car object with sigle value.
+  model: ['juke', 'GT-R', 'sentra'], // - this is a secon example is for the model with a multiple value using array.
   year: '2015',
   getPrice: function () {
-    return 1,000,000;
+    return 1000000;
   },
   printDescription: function () {
     console.log(this.make + ' ' + this.model);
@@ -18,4 +19,15 @@ let car = {
 }
 
 car.printDescription()
-console.log(car.year);
+console.log(car.getPrice());
+console.log(carOne[0]); // - this is referring for the line 6.
+console.log(car['year']);
+console.log(car['make']);
+console.log(car['getPrice']);
+console.log(car.model[0]); // - we call the car object and access the model property and access the array element 2 that has a value of sntra.
+// console.log(car[0]); - do not do this, this is the car object
+/* 
+change the attribute model to array
+*/
+
+// var car = {}
