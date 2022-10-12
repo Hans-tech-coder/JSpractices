0825d190e640e6496b8179c69af3129d679a903d
Hans-tech-coder <obenahans@gmail.com>
2022-10-12 21:07:35 +0800

    add arrow function

---
+++ arrow-function.js
@@ -0,0 +1,9 @@
+/* let hi = () => {console.log('Hello');}
+hi(); */
+
+let hi = (name) => {console.log(`Hello ${name}`)};
+hi (true);
+hi('Hans');
+
+/* let add = (a,b) => {return a + b}
+console.log(add(1,2)); */
